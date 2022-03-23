d208937ac15ef6fd3f7803b32509665668aec53f
Aromal S Ajithan <94230733+Aromal-AS@users.noreply.github.com>
2022-03-23 11:22:09 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+  class Sum
+     {
+        public static void main(String[] args)
+           {
+              int a,b,c;
+              Scanner s = new Scanner(System.in);
+              System.out.println("Enter the first number:");
+              a= s.nextInt();
+              System.out.println("Enter the second number:");
+              b= s.nextInt();
+              c= a + b;
+              System.out.println("The sum of " +a +" and "+ b+ " is: " + c);
+             }
+
+
+     }
