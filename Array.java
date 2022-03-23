3d2db7f04a41a6b4ea0dbdecd5143239173832fd
Aromal S Ajithan <94230733+Aromal-AS@users.noreply.github.com>
2022-03-23 11:44:31 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Array
+{
+public  static void main(String[] args)
+ 
+{
+  
+int n;
+System.out.println("enter the size of array:");
+ Scanner input=new Scanner(System.in);
+ n=input.nextInt();
+int a[]=new int[n];
+ 
+int i;
+ 
+System.out.println("enter the number to be inserted:");
+  
+for(i=0;i<n;i++)
+  
+{
+     
+  a[i]=input.nextInt();
+    
+ }
+   
+ System.out.println("array elements are:");
+   
+ for(i=0;i<n;i++)
+    
+{
+  
+ System.out.println(a[i]);
+  
+}
+ 
+}
+
+}
