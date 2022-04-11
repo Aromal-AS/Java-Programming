4fdac49b521d8697d8231305322ed1ef5eae1ca1
Aromal S Ajithan <94230733+Aromal-AS@users.noreply.github.com>
2022-04-11 14:45:59 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class AreaCircle
+{
+	double area;
+	AreaCircle(double r)
+	{
+	 area= (3.14285714*r*r);
+
+	}
+   public static void main(String[] args) 
+    {   
+      Scanner s= new Scanner(System.in);
+      System.out.println("Enter the radius:");
+      double rad= s.nextDouble();      
+      AreaCircle  a=new AreaCircle(rad);
+      System.out.println("Area of Circle is: " + a.area);      
+   }
+} 
